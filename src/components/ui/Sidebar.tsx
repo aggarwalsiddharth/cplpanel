1e16fc3d801de28a64a406054f5abfc149690b48
aggarwal.siddharth1 <siddharth@Siddharths-MacBook-Air.local>
2024-06-09 23:34:52 +0530

    side bar added

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,6 +1,73 @@
-import Menulist from "./Menulist";
+import MenuComponent from "./Menulist";
 import Useritem from "./Useritem";
+import { Clipboard, Factory, ScrollText, ShoppingCart } from "lucide-react";
 
+const menuList = [
+  {
+    group: "Purchase",
+    icon: <Factory />,
+    items: [
+      {
+        link: "/",
+        text: "Raw Material",
+        icon: <Factory />,
+      },
+      {
+        link: "/",
+        text: "Plain Board",
+        icon: <Clipboard />,
+      },
+      {
+        link: "/",
+        text: "Paper",
+        icon: <ScrollText />,
+      },
+    ],
+  },
+  {
+    group: "Sale",
+    icon: <Clipboard />,
+    items: [
+      {
+        link: "/",
+        text: "Plain Board2",
+        icon: <Factory />,
+      },
+      {
+        link: "/",
+        text: "Plain Board3",
+        icon: <Clipboard />,
+      },
+      {
+        link: "/",
+        text: "Paper1",
+        icon: <ScrollText />,
+      },
+    ],
+  },
+
+  {
+    group: "Settings",
+    icon: <ScrollText />,
+    items: [
+      {
+        link: "/",
+        text: "Raw Material11",
+        icon: <Factory />,
+      },
+      {
+        link: "/",
+        text: "Plain Board12",
+        icon: <Clipboard />,
+      },
+      {
+        link: "/",
+        text: "Paper12",
+        icon: <ScrollText />,
+      },
+    ],
+  },
+];
 const Sidebar = () => {
   return (
     <div className="fixed flex min-h-screen w-[300px] min-w-[300px] flex-col border-r p-1">
@@ -8,7 +75,7 @@ const Sidebar = () => {
         <Useritem />
       </div>
       <div className="flex-grow overflow-hidden">
-        <Menulist />
+        <MenuComponent menuList={menuList} />
       </div>
       <div>Notifications</div>
     </div>
